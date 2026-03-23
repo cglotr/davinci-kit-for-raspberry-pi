390849a24a6feec9279885c0da452143ec28a920
cglotr <aaryfn@gmail.com>
2026-03-22 21:15:20 -0700

    rgbs

--- c/1.1.2/1.1.2_rgbLed.c
+++ c/1.1.2/1.1.2_rgbLed.c
@@ -9,50 +9,50 @@
 #include <wiringPi.h>
 #include <softPwm.h>
 #include <stdio.h>
-#define uchar unsigned char
-#define LedPinRed    0
-#define LedPinGreen  1
-#define LedPinBlue   2
+
+#define uchar    unsigned char
+
+#define GPIO_17  0
+#define GPIO_27  2
+#define GPIO_22  3
+
+#define DELAY_MS 1000
 
 void ledInit(void){
-    softPwmCreate(LedPinRed,  0, 100);
-    softPwmCreate(LedPinGreen,0, 100);
-    softPwmCreate(LedPinBlue, 0, 100);
+    softPwmCreate(GPIO_17, 0, 100);
+    softPwmCreate(GPIO_27, 0, 100);
+    softPwmCreate(GPIO_22, 0, 100);
 }
 
 void ledColorSet(uchar r_val, uchar g_val, uchar b_val){
-    softPwmWrite(LedPinRed,   r_val);
-    softPwmWrite(LedPinGreen, g_val);
-    softPwmWrite(LedPinBlue,  b_val);
+    softPwmWrite(GPIO_17, r_val);
+    softPwmWrite(GPIO_27, g_val);
+    softPwmWrite(GPIO_22, b_val);
 }
 
 int main(void){
-
-    if(wiringPiSetup() == -1){ //when initialize wiring failed, printf messageto screen
+    if(wiringPiSetup() == -1) {
         printf("setup wiringPi failed !");
         return 1;
     }
 
     ledInit();
-    while(1){
-        printf("Red\n");
-        ledColorSet(0xff,0x00,0x00);   //red     
-        delay(500);
-        printf("Green\n");
-        ledColorSet(0x00,0xff,0x00);   //green
-        delay(500);
-        printf("Blue\n");
-        ledColorSet(0x00,0x00,0xff);   //blue
-        delay(500);
-        printf("Yellow\n");
-        ledColorSet(0xff,0xff,0x00);   //yellow
-        delay(500);
-        printf("Purple\n");
-        ledColorSet(0xff,0x00,0xff);   //purple
-        delay(500);
-        printf("Cyan\n");
-        ledColorSet(0xc0,0xff,0x3e);   //cyan
-        delay(500);
+
+    uchar rgb_run[6][3] = {
+        { 0xff, 0x00, 0x00 }, // red,
+        { 0x00, 0xff, 0x00 }, // green,
+        { 0x00, 0x00, 0xff }, // blue,
+        { 0xff, 0xff, 0x00 }, // yellow,
+        { 0xff, 0x00, 0xff }, // purple,
+        { 0x00, 0xff, 0xff } // cyan
+    };
+
+    while (1) {
+        for (int i = 0; i < sizeof(rgb_run) / sizeof(rgb_run[0]); i++) {
+            ledColorSet(rgb_run[i][0], rgb_run[i][1], rgb_run[i][2]);
+            delay(DELAY_MS);
+        }
     }
+
     return 0;
 }
