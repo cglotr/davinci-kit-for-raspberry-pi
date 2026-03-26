532926f8ddbe97c01e968a9a99fee53ea432477b
cglotr <aaryfn@gmail.com>
2026-03-26 05:48:57 -0700

    format 5

--- c/1.1.5/1.1.5_4-Digit.c
+++ c/1.1.5/1.1.5_4-Digit.c
@@ -12,29 +12,48 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define SDI 5
-#define RCLK 4
+#define SDI   5
+#define RCLK  4
 #define SRCLK 1
 
-const int placePin[] = {12, 3, 2, 0};
-unsigned char number[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+#define GPIO_17 0
+#define GPIO_18 1
+#define GPIO_22 3
+#define GPIO_23 4
+#define GPIO_26 12
+#define GPIO_27 2
+
+const int placePin[] = {
+    GPIO_26,
+    GPIO_22,
+    GPIO_27,
+    GPIO_17
+};
+unsigned char number[] = {
+    0xc0,
+    0xf9,
+    0xa4,
+    0xb0,
+    0x99,
+    0x92,
+    0x82,
+    0xf8,
+    0x80,
+    0x90
+};
 
 int counter = 0;
 
-void pickDigit(int digit)
-{
-    for (int i = 0; i < 4; i++)
-    {
+void pickDigit(int digit) {
+    for (int i = 0; i < 4; i++) {
         digitalWrite(placePin[i], 0);
     }
     digitalWrite(placePin[digit], 1);
 }
 
-void hc595_shift(int8_t data)
-{
+void hc595_shift(int8_t data) {
     int i;
-    for (i = 0; i < 8; i++)
-    {
+    for (i = 0; i < 8; i++) {
         digitalWrite(SDI, 0x80 & (data << i));
         digitalWrite(SRCLK, 1);
         delayMicroseconds(1);
@@ -45,11 +64,9 @@ void hc595_shift(int8_t data)
     digitalWrite(RCLK, 0);
 }
 
-void clearDisplay()
-{
+void clearDisplay() {
     int i;
-    for (i = 0; i < 8; i++)
-    {
+    for (i = 0; i < 8; i++) {
         digitalWrite(SDI, 1);
         digitalWrite(SRCLK, 1);
         delayMicroseconds(1);
@@ -60,54 +77,51 @@ void clearDisplay()
     digitalWrite(RCLK, 0);
 }
 
-void loop()
-{
-    while(1){
-    clearDisplay();
-    pickDigit(0);
-    hc595_shift(number[counter % 10]);
+void loop() {
+    while (1) {
+        clearDisplay();
+        pickDigit(0);
+        hc595_shift(number[counter % 10]);
 
-    clearDisplay();
-    pickDigit(1);
-    hc595_shift(number[counter % 100 / 10]);
+        clearDisplay();
+        pickDigit(1);
+        hc595_shift(number[counter % 100 / 10]);
 
-    clearDisplay();
-    pickDigit(2);
-    hc595_shift(number[counter % 1000 / 100]);
- 
-    clearDisplay();
-    pickDigit(3);
-    hc595_shift(number[counter % 10000 / 1000]);
+        clearDisplay();
+        pickDigit(2);
+        hc595_shift(number[counter % 1000 / 100]);
+
+        clearDisplay();
+        pickDigit(3);
+        hc595_shift(number[counter % 10000 / 1000]);
     }
 }
 
-void timer(int timer1)
-{ 
-    if (timer1 == SIGALRM)
-    { 
+void timer(int t) {
+    if (t == SIGALRM) {
         counter++;
-        alarm(1); 
+        alarm(1);
         printf("%d\n", counter);
     }
 }
 
-void main(void)
-{
-    if (wiringPiSetup() == -1)
-    { 
+void main(void) {
+    if (wiringPiSetup() == -1) {
         printf("setup wiringPi failed !");
         return;
     }
-    pinMode(SDI, OUTPUT); 
-    pinMode(RCLK, OUTPUT);
+
+    pinMode(SDI,   OUTPUT);
+    pinMode(RCLK,  OUTPUT);
     pinMode(SRCLK, OUTPUT);
-    
-    for (int i = 0; i < 4; i++)
-    {
+
+    for (int i = 0; i < 4; i++) {
         pinMode(placePin[i], OUTPUT);
         digitalWrite(placePin[i], HIGH);
     }
-    signal(SIGALRM, timer); 
-    alarm(1);               
-    loop(); 
+
+    signal(SIGALRM, timer);
+    alarm(1);
+
+    loop();
 }
